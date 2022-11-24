05170d8812625953131bdf768ea725786ce23f5e
Hunter Welch <35479403+hwelch@users.noreply.github.com>
2022-11-23 19:54:57 -0500

    Added objective function logic
    added objective function logic

--- P2/ex1.py
+++ P2/ex1.py
@@ -23,5 +23,56 @@ def parse_csv():
         return irises
 
 
-arr = np.array(parse_csv())
-print(arr)
+data = np.array(parse_csv())
+print(data)
+
+# global vars
+k = 1
+# mus will be an array of k Irises with their species set to mean 0-k-1
+mus = np.array([])
+# each cluster has its own array of elements it contains
+data_k0 = np.array([])
+data_k1 = np.array([])
+data_k2 = np.array([])
+
+
+# helper function to calculate distortion
+def distortion(mu, x):
+    d = 0
+    d += (mu.sepal_length - x.sepal_length) ** 2
+    d += (mu.sepal_width - x.sepal_width) ** 2
+    d += (mu.petal_length - x.petal_length) ** 2
+    d += (mu.petal_width - x.petal_width) ** 2
+    return d
+
+
+def place_data(x, cluster):
+    if cluster == 0:
+        global data_k0
+        data_k0 = np.append(data_k0, x)
+    if cluster == 1:
+        global data_k1
+        data_k1 = np.append(data_k1, x)
+    if cluster == 2:
+        global data_k2
+        data_k2 = np.append(data_k2, x)
+
+
+# the objective function both sums the total distortion and classifies the data
+def classify_data():
+    total = 0
+    for datapt in data:
+        count = 0
+        lowest = 100000000
+        k_class = -1
+        while count < k:
+            d = distortion(mus[count], datapt)
+            if d < lowest:
+                lowest = d
+                k_class = count
+            count += 1
+        place_data(datapt, k_class)
+        total += lowest
+    return total
+
+
